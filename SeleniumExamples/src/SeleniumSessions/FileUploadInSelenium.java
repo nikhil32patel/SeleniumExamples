f79e33ed28acec8994b1b030c0407f8d8c3bb5e3
nikhil32patel <nikhil32patel@gmail.com>
2022-12-15 18:26:56 +0530

    Web driver manager code added

--- SeleniumExamples/src/SeleniumSessions/FileUploadInSelenium.java
+++ SeleniumExamples/src/SeleniumSessions/FileUploadInSelenium.java
@@ -13,16 +13,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import pac.config.Config;
+
 public class FileUploadInSelenium {
 
 	public static void main(String[] args) throws AWTException {
 		// TODO Auto-generated method stub
 
-		String ChromeDriver ="S:\\SOFTWARES\\Selenium\\WebDrivers\\Chrome\\Chromedriver_99.0.4844.51\\chromedriver.exe"; 
-		System.setProperty("webdriver.chrome.driver",ChromeDriver);
-
-		WebDriver driver = new ChromeDriver();
+		/*
+		 * String ChromeDriver
+		 * ="S:\\SOFTWARES\\Selenium\\WebDrivers\\Chrome\\Chromedriver_99.0.4844.51\\chromedriver.exe";
+		 * System.setProperty("webdriver.chrome.driver",ChromeDriver); WebDriver driver
+		 * = new ChromeDriver();
+		 */
 		
+		WebDriver driver = Config.startBrowser("chrome");
 		driver.get("https://www.monsterindia.com/");
 		
 		driver.manage().window().maximize();
