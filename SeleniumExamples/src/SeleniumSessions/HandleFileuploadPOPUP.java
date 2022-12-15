f3babb330614b2981f86e86788db3ae2444ce49e
nikhil32patel <nikhil32patel@gmail.com>
2022-12-15 18:27:33 +0530

    WebDriver manager code added to handle latest chrome driver

--- SeleniumExamples/src/SeleniumSessions/HandleFileuploadPOPUP.java
+++ SeleniumExamples/src/SeleniumSessions/HandleFileuploadPOPUP.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import pac.config.Config;
+
 public class HandleFileuploadPOPUP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		 System.setProperty("webdriver.gecko.driver","C:\\Users\\NIKHIL BHANVADIYA\\Downloads\\geckodriver-v0.29.1-win64\\geckodriver.exe");
-		 WebDriver driver = new FirefoxDriver();
+//		 System.setProperty("webdriver.gecko.driver","C:\\Users\\NIKHIL BHANVADIYA\\Downloads\\geckodriver-v0.29.1-win64\\geckodriver.exe");
+//		 WebDriver driver = new FirefoxDriver();
 		
 		// HOW TO LUNCH CHROME BROWSWER
 		/*
@@ -19,11 +21,11 @@ public class HandleFileuploadPOPUP {
 		 * ); WebDriver driver = new ChromeDriver();
 		 * driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
 		 */
-		
-		// To handle alerts and windows pop up messaages using alert API
-		driver.get("https://html.com/input-type-file/");
-		driver.findElement(By.id("fileupload")).sendKeys("C:\\Users\\NIKHIL BHANVADIYA\\Desktop\\Test.html");
-		
-	}
 
+		// To handle alerts and windows pop up messaages using alert API
+		WebDriver driver = Config.startBrowser("chrome");
+		driver.get("https://html.com/input-type-file/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("fileupload")).sendKeys("C:\\Users\\NIKHIL BHANVADIYA\\Desktop\\Test.html");
+	}
 }
