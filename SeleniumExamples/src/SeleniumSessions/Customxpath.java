bd81aa46172bd79031d3f56e589f1e09a227260a
nikhil32patel <nikhil32patel@gmail.com>
2022-12-15 18:26:40 +0530

    Web driver manager code added

--- SeleniumExamples/src/SeleniumSessions/Customxpath.java
+++ SeleniumExamples/src/SeleniumSessions/Customxpath.java
@@ -6,14 +6,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import pac.config.Config;
+
 public class Customxpath {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","S:\\SOFTWARES\\Selenium\\WebDrivers\\Chrome\\ChromeDriver 93.0.4577.63\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.half.ebay.com/");
+//		System.setProperty("webdriver.chrome.driver","S:\\SOFTWARES\\Selenium\\WebDrivers\\Chrome\\ChromeDriver 93.0.4577.63\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
 		
+		
+		WebDriver driver = Config.startBrowser("chrome");
+		driver.get("http://www.half.ebay.com/");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		
