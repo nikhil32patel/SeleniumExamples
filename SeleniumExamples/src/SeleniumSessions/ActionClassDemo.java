5c8bd4da65ee2ae21bcd3246683cb7b0d9ce11c1
nikhil32patel <nikhil32patel@gmail.com>
2022-12-15 18:26:19 +0530

    WebDriver manager code added

--- SeleniumExamples/src/SeleniumSessions/ActionClassDemo.java
+++ SeleniumExamples/src/SeleniumSessions/ActionClassDemo.java
@@ -8,28 +8,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
+import pac.config.Config;
+
 
 public class ActionClassDemo {
 	
 	
 	public static void main(String[] args) throws InterruptedException
 	{
-		String ChromeDriver ="S:\\SOFTWARES\\Selenium\\WebDrivers\\Chrome\\Chromedriver_99.0.4844.51\\chromedriver.exe"; 
-		System.setProperty("webdriver.chrome.driver",ChromeDriver);
-		
-		ChromeOptions options = new ChromeOptions();
-		//options.addArguments("disable-extensions");
-		//options.setExperimentalOption("useAutomationExtension", false);
-		
-		//chrome is being controled by automated software
-		options.setExperimentalOption("excludeSwitches",Collections.singletonList("enable-automation"));    
-		WebDriver driver = new ChromeDriver(options);
-		//WebDriver driver = new ChromeDriver();
+		/*
+		 * String ChromeDriver
+		 * ="S:\\SOFTWARES\\Selenium\\WebDrivers\\Chrome\\Chromedriver_99.0.4844.51\\chromedriver.exe";
+		 * System.setProperty("webdriver.chrome.driver",ChromeDriver);
+		 * 
+		 * ChromeOptions options = new ChromeOptions();
+		 * //options.addArguments("disable-extensions");
+		 * //options.setExperimentalOption("useAutomationExtension", false);
+		 * 
+		 * //chrome is being controled by automated software
+		 * options.setExperimentalOption("excludeSwitches",Collections.singletonList(
+		 * "enable-automation")); WebDriver driver = new ChromeDriver(options);
+		 */
+	
+		WebDriver driver = Config.startBrowser("chrome");
 		driver.get("http://mrbool.com/");
-		
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		
 		
 		// TO MOUSE HOVER
 		Actions act = new Actions(driver);
@@ -38,6 +42,5 @@ public class ActionClassDemo {
 		
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//ul[@class='submenu']//li//a[text()='Articles']")).click();
-		
 	}
 }
